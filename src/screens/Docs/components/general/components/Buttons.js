2e663c3e7443c1f566019888b4f1a2d8e3a7480c
Aleem Isiaka <aleemisiaka@gmail.com>
2018-10-11 13:55:16 +0100

    Completes codebox for showing codes

--- src/screens/Docs/components/general/components/Buttons.js
+++ src/screens/Docs/components/general/components/Buttons.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import ButtonExamples from './components/ButtonExamples';
 
 const Buttons = (props) => {
   return (
-    <div>Trial</div>
+    <article>
+      <section>
+        <h1>Button</h1>
+      </section>
+      <section>
+        <p>To trigger an option</p>
+        <h2 id="when-to-use">When To Use</h2>
+        <p>A button means an operation (or a series of operations). Clicking a button will trigger corresponding business logic.</p>
+      </section>
+      <h2>Examples</h2>
+      <ButtonExamples />
+    </article>
   );
 }
 
